dfa481d0e1704236dde5a706bf36566b4c60e0d8
mchgbnath <35652847+mchgbnath@users.noreply.github.com>
2018-01-25 14:52:08 +0000

    Rename vowel.c to vowel or consonent.c

---
+++ vowel or consonent.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     char a;
+     clrscr();
+     printf("Enter the letter\n");
+     scanf("%c",&a);    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+     printf("The letter is vowel");
+     else
+     printf("The letter is consonent");
+     getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
